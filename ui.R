d134b3fa4f6a5f2ce2fad3223b233b3d71223cb8
Ismael Fernandez <ismf_93@ciencias.unam.mx>
2015-08-24 10:59:53 -0500

    Added web skeleton

--- ui.R
+++ ui.R
@@ -8,23 +8,27 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
+  titlePanel(h1("Accounting")),
+  fluidRow(
+    
+    column(3,
+           dateRangeInput("dates", label = h3("Date range"))),
+    
+    column(3, 
+           textInput("text", label = h3("Text input"), 
+                     value = "Enter text..."))
+  ),  
   sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+    sidebarPanel(  checkboxGroupInput("checkGroup", 
+                                      label = h3("Checkbox group"), 
+                                      choices = list("Choice 1" = 1, 
+                                                     "Choice 2" = 2, "Choice 3" = 3),
+                                      selected = 1),  
+                   selectInput("select", label = h3("Select box"), 
+                                                                  choices = list("Choice 1" = 1, "Choice 2" = 2,
+                                                                                 "Choice 3" = 3), selected = 1)),
+    mainPanel("main panel")
   )
+
+  
 ))
